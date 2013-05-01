30eeafd073f43e97d2ac31af545ce183cc2db718
Justin Ridgewell <justin@ridgewell.name>
2013-05-01 03:13:36 -0400

    Keybindings

--- home/.zsh/config/keybindings.zsh
+++ home/.zsh/config/keybindings.zsh
@@ -2,6 +2,14 @@
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
 
+# Alt + ArrowKeys
+bindkey '^[b' backward-word
+bindkey '^[f' forward-word
+
+# Emacs Ctrl-A and Ctrl-E
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
+
 # Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
 bindkey '^?' backward-delete-char
 bindkey "^[[3~" delete-char
