6822dec354212ea6e024fb737ca31f6810a11d65
Justin Ridgewell <justin@ridgewell.name>
2013-06-02 00:36:11 -0400

    Refactor tat function.
    * Adds in ability to create new session if it doesn't exist.
    * Adds in tab completion for created sessions

--- home/.zsh/functions/tat.zsh
+++ home/.zsh/functions/tat.zsh
@@ -2,5 +2,15 @@
 # If no session is specified, default
 # to the current directory
 function tat() {
-    tmux attach -t ${1:=`basename $PWD`}
+    local session=${1:=${PWD##*/}}
+    tmux has -t $session && tmux attach -t $session || tmux new -s $session
 }
+
+function __tmux-sessions() {
+    local expl
+    local -a sessions
+    sessions=( ${${(f)"$(command tmux list-sessions)"}/:[ $'\t']##/:} )
+    _describe -t sessions 'sessions' sessions "$@"
+}
+
+compdef __tmux-sessions tat
