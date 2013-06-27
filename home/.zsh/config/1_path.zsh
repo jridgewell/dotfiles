fa7603f892c6029b945c5220593d69179df3c05e
Justin Ridgewell <jridgewell@cloudspace.com>
2013-06-27 10:27:05 -0400

    Don't echo. Oops.

--- home/.zsh/config/1_path.zsh
+++ home/.zsh/config/1_path.zsh
@@ -2,7 +2,6 @@ typeset -aU path
 
 # Only load PATH once
 if [ -z $PATH_LOADED ]; then
-    echo 'loading path'
     path=($HOME/.phpenv/bin $path)
     path=($HOME/.rbenv/bin $path)
     path=(/usr/local/bin /usr/local/sbin:$path)
