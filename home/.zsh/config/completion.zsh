c5e4f9136565309701260559fd1e44ed13a58bf4
Justin Ridgewell <justin@ridgewell.name>
2014-07-18 19:35:47 -0400

    Add more zsh tab completions

--- home/.zsh/config/completion.zsh
+++ home/.zsh/config/completion.zsh
@@ -4,7 +4,12 @@ setopt auto_menu
 setopt complete_in_word
 setopt always_to_end
 
-fpath=(~/.zsh/completions $fpath)
+fpath=(
+    ~/.zsh/completions
+    /usr/local/share/zsh/site-functions
+    /usr/local/share/zsh-completions
+    $fpath
+)
 autoload -U compinit && compinit -i
 
 compdef hub=git
