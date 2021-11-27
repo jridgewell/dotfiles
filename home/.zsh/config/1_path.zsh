d707cf473cf9d0386fe3a0fea4d1ada39351ba2f
Justin Ridgewell <justin@ridgewell.name>
2021-11-27 04:35:48 -0500

    Cleanup old languages

--- home/.zsh/config/1_path.zsh
+++ home/.zsh/config/1_path.zsh
@@ -3,11 +3,8 @@ typeset -aU path
 # Only load PATH once
 if [ -z $PATH_LOADED ]; then
     path=($HOME/homebrew/bin $path)
-    path=($HOME/.phpenv/bin $path)
-    path=($HOME/.rbenv/bin $path)
     path=($HOME/.nvm $path)
     path=($HOME/.cargo/bin $path)
-    path=(/usr/local/share/npm/bin $path)
     path=(/usr/local/bin /usr/local/sbin $path)
     path=($HOME/.zsh/bin $path)
     path=($HOME/bin $path)
--- home/.zsh/config/completion.zsh
+++ home/.zsh/config/completion.zsh
@@ -4,12 +4,20 @@ setopt auto_menu
 setopt complete_in_word
 setopt always_to_end
 
+if type brew &>/dev/null; then
+    local HOMEBREW_PREFIX="$(brew --prefix)"
+    fpath=(
+        $HOMEBREW_PREFIX/share/zsh-completions
+        $HOMEBREW_PREFIX/share/zsh/site-functions
+        $fpath
+    )
+fi
+
 fpath=(
     ~/.zsh/completions
-    /usr/local/share/zsh/site-functions
-    /usr/local/share/zsh-completions
     $fpath
 )
-autoload -U compinit && compinit -i
+
+autoload -Uz compinit && compinit -i
 
 compdef hub=git
