840fa64a3e46b8d7ad2e492ba70832d6cafaa422
Justin Ridgewell <justin@ridgewell.name>
2022-07-13 12:51:22 -0400

    Setup fnm

---
+++ home/.zsh/config/node.zsh
@@ -0,0 +1,38 @@
+##########################
+### Language Specific ####
+##########################
+# Node.js
+if is-callable fnm; then
+    export FNM_DIR="$HOME/.fnm"
+    eval "$(fnm env --use-on-cd)"
+elif is-callable nvm-exec; then
+    export NVM_DIR="$HOME/.nvm"
+    # source "$NVM_DIR/nvm.sh"
+    lazy_load_nvm() {
+        unset -f node npm npx yarn nvm
+        [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+    }
+
+    node() {
+        lazy_load_nvm
+        node $@
+    }
+    npm() {
+        lazy_load_nvm
+        npm $@
+    }
+    npx() {
+        lazy_load_nvm
+        npx $@
+    }
+    yarn() {
+        lazy_load_nvm
+        yarn $@
+    }
+    nvm() {
+        lazy_load_nvm
+        nvm $@
+    }
+else
+    export NODE_PATH="/usr/local/lib/node_modules:$NODE_PATH"
+fi
